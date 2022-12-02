9f2305eb4b7c83dd5765b3dd0f4e51fe80b45d1e
Tomasz Rafalski <tomekrafalski1@gmail.com>
2022-12-02 12:04:59 +0100

    AoC day 1 and day 2 answers

---
+++ src/main/java/AoC2022Core.java
@@ -0,0 +1,9 @@
+public class AoC2022Core {
+
+    public static void main(String[] args) {
+        new Day1();
+        System.out.println();
+        new Day2();
+        System.out.println();
+    }
+}
---
+++ src/main/java/Day1.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day1 extends AOCPuzzle{
+
+    public Day1()
+    {
+        super("1");
+    }
+
+    @Override
+    void solve(List<String> input) {
+        int max = 0;
+        int sum = 0;
+        for (String s : input) {
+            if (!s.isBlank()) {
+                sum += Integer.parseInt(s);
+            } else {
+                if (sum > max) {
+                    max = sum;
+                }
+                sum = 0;
+            }
+        }
+        System.out.println("Day 1 First Answer: " + max);
+
+        List<Integer> topThree = new ArrayList<>();
+        sum = 0;
+        for (String s : input) {
+            if (!s.isBlank()) {
+                sum += Integer.parseInt(s);
+            } else if (topThree.size() < 3) {
+                topThree.add(sum);
+                sum = 0;
+            } else {
+                int lowestIndex = topThree.indexOf(Collections.min(topThree));
+                int lowest = topThree.get(lowestIndex);
+                if (sum > lowest) {
+                    topThree.set(lowestIndex, sum);
+                }
+                sum = 0;
+            }
+        }
+        max = topThree.stream().mapToInt(Integer::intValue).sum();
+
+        System.out.println("Day 1 Second Answer: " + max);
+
+    }
+}
